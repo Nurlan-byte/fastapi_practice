bbaa07e495f71a209d498fb9a7bbe27ece962c65
Yussupov Nurlan <noooorlan2007@gmail.com>
2026-07-30 16:45:02 +0500

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import psycopg
 from psycopg.rows import dict_row
 from .database import Base, engine
@@ -8,6 +9,16 @@ from .routers import post, user, auth, vote
 # Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # try:
 #     conn = psycopg.connect(
@@ -27,5 +38,5 @@ app.include_router(vote.router)
 
 @app.get("/") # get это один из методов HTTP, / это путь от наччального адреса например от http://127.0.0.1:8000 и это ожно и тоже с http://127.0.0.1:8000/
 async def root(): #async  опционально 
-    return {"message": "Hello World"} #формат json
+    return {"message": "Hello World!!!!"} #формат json
 
